ed20fe3feeff334d162c048dca1ac5ecdca09687
Idris <kpakisnique1000@gmail.com>
2024-05-30 14:57:14 +0200

    add Agent file

---
+++ Agent.py
@@ -0,0 +1,9 @@
+class Agent():
+    def __init__(self, strategy, num_actions, device):
+        self.current_step = 0
+        self.strategy = strategy
+        self.num_actions = num_actions
+        self.device = device
+
+    def select_action(self, state, policy_net):
+       pass
