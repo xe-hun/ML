ccce4fd29dae17fc618aefb129c119945e24b435
Idris <Kpakisnique@gmail.com>
2024-06-01 19:21:50 +0100

    modified policy.py

--- Policy.py
+++ Policy.py
@@ -1,48 +1,69 @@
 import numpy as np
 import random
 class Policy:
-    def  __init__(self, strategy):
+    def  __init__(self):
        pass
     
-    def initializeQValues(self, stateSpace, numberOfAction:int):
+    def initializeQValues(self, stateSpace, actionSpaceSize:int):
         self.Q = {}
-        self.numberOfAction = numberOfAction
+        self.actionSpaceSize = actionSpaceSize
         
         initialQValue = 0
         for s in stateSpace:
-                self.Q[s] = [initialQValue for _ in range(numberOfAction)]
+                self.Q[s] = [initialQValue for _ in range(actionSpaceSize)]
         return self.Q
     
-    def getAction(self, state)->int:
-        return np.argmax(self.Q[state])
+    def getMaxAction(self, state)->int:
+        return self.randomTieBreakArgmax(self.Q[state])
     
     def getRandomAction(self)->int:
-        return random.randrange(self.numberOfAction);
+        return random.randrange(self.actionSpaceSize);
         # return random.choice(list(range(self.numberOfAction)));
     
     def getActionWithStrategy(self, explorationRate:float, state:tuple):
         if(explorationRate > random.random()):
             return self.getRandomAction()
         else:
-            return self.getAction(state)
+            return self.getMaxAction(state)
     
-    def train(self, currentState:tuple, nextState:tuple, reward:int, action:int):
+    def train(self, currentState:int, nextState:int, reward:int, action:int):
         if(currentState == nextState):
             return
         
         alpha = .01 #learningRate
         gamma = .9 #discountFactor
-        self.Q[currentState][action] = self.Q[currentState][action] + alpha * ((reward + gamma * max(self.Q[nextState])) - self.Q[currentState][action]) 
+        qsetter = self.Q[currentState][action] + alpha * (reward + gamma * max(self.Q[nextState]) - self.Q[currentState][action])
+        # if(reward > 0):
+        #     print(qsetter)
+        self.Q[currentState][action] = qsetter
         
+
+    def randomTieBreakArgmax(self, arr):
     
-    # def updateQValue(self):
-    #     pass
-    
-    
-    # # def train(sel):
-    
-    # def getQValue(self, state):
-    #     # if(self.Q == None) 
-    #     # then you must initialize q value first
-    #     return self.Q[state]
+        max_value = max(arr)
+        # Find indices of all elements equal to the maximum value
+        max_indices = [i for i, x in enumerate(arr) if x == max_value]
+
+        # If there's only one maximum, return its index directly
+        if len(max_indices) == 1:
+            return max_indices[0]
+
+        # Randomly choose one index from the tie
+        random_index = random.choice(max_indices)
+        return random_index
         
+        
+            # def randomTieBreakArgmax(self, arr):
+ 
+    #     # Find the maximum value(s)
+    #     max_value = arr.max()
+    #     # Find indices of all elements equal to the maximum value
+    #     max_indices = np.flatnonzero(arr == max_value)
+
+    #     # If there's only one maximum, return its index directly
+    #     if len(max_indices) == 1:
+    #         return max_indices[0]
+
+    #     # Randomly choose one index from the tie
+    #     random_index = np.random.choice(max_indices)
+    #     return random_index
